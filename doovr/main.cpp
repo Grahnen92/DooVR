1591a431f8f88236b0cf2902ef63ed0c5c76fe9d
jonathanbosson <jonathan.bosson@gmail.com>
2015-02-25 11:00:07 +0100

    OpenGL ver support for OS X

--- doovr/main.cpp
+++ doovr/main.cpp
@@ -39,6 +39,13 @@ int main()
 		return 1;
 	}
 
+    
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
 	//create GLFW window and select context
 	GLFWwindow* window = glfwCreateWindow(640, 480, "Do not try and bend the spoon. That's impossible. Instead... only try to realize the truth.", NULL, NULL);
 	if (!window) {
